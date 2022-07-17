1f8ed4fc138f193cda8883ae3a6d288fa12a1dd0
Станислав <qpoto@yandex.ru>
2022-07-17 18:29:19 +0300

    0. Операции сравнения [#235631 #322798]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -6,10 +6,8 @@ public class Liken {
         int second = 9;
         boolean result = first > second;
                 System.out.println(result);
-
                 result = first < second;
                 System.out.println(result);
-
                 result = first == second;
                 System.out.println(result);
     }
