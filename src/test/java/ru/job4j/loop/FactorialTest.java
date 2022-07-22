623f2fd04eb8ba995e2451113a5147a0d68824ed
Станислав <qpoto@yandex.ru>
2022-07-22 09:29:46 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #324455]

--- src/test/java/ru/job4j/loop/FactorialTest.java
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 public class FactorialTest {
     @Test
     public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
-        int start = 1;
-        int finish = 5;
         int result = Factorial.calc(5);
         int expected = 120;
         Assert.assertEquals(expected, result);
@@ -15,8 +13,6 @@ public class FactorialTest {
 
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        int start = 1;
-        int finish = 0;
         int result = Factorial.calc(0);
         int expected = 1;
         Assert.assertEquals(expected, result);
