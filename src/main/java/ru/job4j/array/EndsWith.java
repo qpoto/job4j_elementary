9930356727fbb00a0ee8f78727aab67d4d09a994
Станислав <qpoto@yandex.ru>
2022-07-31 22:22:52 +0300

    6.4.1. Сравнить последние элементы двух массивов [#304228 #326481]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int i = 0; i < word.length; i++) {
+        for (int i = 0; i < post.length; i++) {
             if (word[word.length - i - 1] != post[post.length - i - 1]) {
                 return false;
             }
